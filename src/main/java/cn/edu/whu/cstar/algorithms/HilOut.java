1bba7ca46d0b3b3a4a4d2657240ca17599d4bd65
Gu-youngfeng <yongfeng_gu@163.com>
2017-11-10 20:32:32 +0800

    Update HilOut.java
    add accuracy getter in function showResults().

--- src/main/java/cn/edu/whu/cstar/algorithms/HilOut.java
+++ src/main/java/cn/edu/whu/cstar/algorithms/HilOut.java
@@ -27,7 +27,7 @@ public class HilOut {
 	/** K-nearest neighbors*/
 	public static final int K = 10;
 	/** top-N outliers*/
-	public static final double N = 0.1;
+	public static final double N = 0.05;
 	
 	private static List<HILNode> nodeset = new ArrayList<HILNode>();
 	
@@ -112,6 +112,7 @@ public class HilOut {
 		System.out.println("PRECISION:" + mc.getPRECISION());
 		System.out.println("RECALL:" + mc.getRECALL());
 		System.out.println("F-MEASURE:" + mc.getFMEASURE());
+		System.out.println("ACCURACY:" + mc.getCORRECTRATIO());
 
 	}
 	
